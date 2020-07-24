60ab81c86f66e2872f7f4770c35134145907f664
abiola farounbi <farounbiabiola@gmail.com>
2020-07-24 02:17:31 +0100

    Updates

---
+++ precache-manifest.8bbeb8b804fcfa99ff4d0b9aa060ba3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68870d623671aa5a3c3f771b9cb040ec",
+    "url": "/Abiola-Farounbi/KarlFashion/index.html"
+  },
+  {
+    "revision": "13a949152cf7c2c297d4",
+    "url": "/Abiola-Farounbi/KarlFashion/static/css/2.1ce3413f.chunk.css"
+  },
+  {
+    "revision": "c931290049f1790e5d2f",
+    "url": "/Abiola-Farounbi/KarlFashion/static/css/main.60ca992a.chunk.css"
+  },
+  {
+    "revision": "13a949152cf7c2c297d4",
+    "url": "/Abiola-Farounbi/KarlFashion/static/js/2.f388b7e0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Abiola-Farounbi/KarlFashion/static/js/2.f388b7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c931290049f1790e5d2f",
+    "url": "/Abiola-Farounbi/KarlFashion/static/js/main.a48dc363.chunk.js"
+  },
+  {
+    "revision": "6fffe4044e8c55622c18",
+    "url": "/Abiola-Farounbi/KarlFashion/static/js/runtime-main.e7ee046e.js"
+  },
+  {
+    "revision": "ca3cc290a8ff0d9a6bb79fcb5fe5c2aa",
+    "url": "/Abiola-Farounbi/KarlFashion/static/media/bg.ca3cc290.jpg"
+  },
+  {
+    "revision": "f4b64d687ae078693b66bc248f388868",
+    "url": "/Abiola-Farounbi/KarlFashion/static/media/logo1.f4b64d68.png"
+  },
+  {
+    "revision": "99a6cd8d9a9dcab799b9b394a996b63c",
+    "url": "/Abiola-Farounbi/KarlFashion/static/media/slider-img.99a6cd8d.png"
+  }
+]);
